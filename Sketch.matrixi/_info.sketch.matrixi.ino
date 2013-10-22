3afdf903b4d7ef3867273e33eb9df0b7ee155f3c
RobinSi <robin.siemiatkowski@IG2I.fr>
2013-10-22 11:03:47 +0200

    add info.txt for install and compilation on Arduino IDE

---
+++ Sketch.matrixi/_info.sketch.matrixi.ino
@@ -0,0 +1,9 @@
+#INSTALL Arduino IDE:
+
+sudo apt-get install arduino
+
+#CONFIGURE Arduino IDE:
+
+open Arduino;
+in Tools> Board: select Arduino;
+in tools> Serial Port: Select port for uploading program to Arduino.
